92f479c005e712058f015ae0ec7809f6dff46247
Z0ro0 <chlrkdmf0805@gmail.com>
2023-04-16 00:00:21 +0900

    이벤트 핸들링 onKeyUp

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ function App() {
   const onSubmit = () =>{
     alert('submitted');
   }
+
+  const onKeyUp = (event) =>{
+    if(event.keyCode===13){//enter를 누르면 함수 실행
+        onSubmit();
+    }
+  }
   return (
     <div className="App">
+      <input onKeyUp={onKeyUp}></input>
       <button onClick={onSubmit}>submit</button>
     </div>
   );
